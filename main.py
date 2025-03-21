b9af9e5a08d948fbc37cb7d437efb3e129f261f1
Dominik_Joers <d.joers@campus.tu-berlin.de>
2025-03-21 13:36:34 +0100

    Lesson CH2 L2 Game Loop

---
+++ main.py
@@ -0,0 +1,33 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+# Define the RGB color for black
+BLACK = (0, 0, 0)
+def main():
+    print("Starting Asteroids!")
+    print("Screen width:",SCREEN_WIDTH)
+    print("Screen height:",SCREEN_HEIGHT)
+    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+   
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(BLACK)
+        pygame.display.flip()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+    #Quit the game when the user closes the window
+    pygame.quit()
+    
+
+if __name__ == "__main__":
+    main()
