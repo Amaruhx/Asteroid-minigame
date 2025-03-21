3635ab0246e1e3dcf4a4e2a7949901b0f70f2319
Dominik_Joers <d.joers@campus.tu-berlin.de>
2025-03-21 19:35:37 +0100

    added Triangle as a Player CH2 L5

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+import player
+
 
 # Define the RGB color for black
 BLACK = (0, 0, 0)
@@ -17,12 +19,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
    
+    # Create a player object
+    player_instance = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+   
    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(BLACK)
+        
+        # Call the draw method on the player instance
+        player_instance.draw(screen)
+        
         pygame.display.flip()
         clock.tick(60)
         
